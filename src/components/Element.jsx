6ba26eede826a4dbc8edb86ec0b0b4433680d6a8
Stalin Acurio <sta_vla411@hotmail.com>
2023-01-01 09:23:18 -0500

    complete page main

--- src/components/Element.jsx
+++ src/components/Element.jsx
@@ -1,22 +1,55 @@
 import React from "react";
 import styled from "styled-components";
-import rock from "../assets/icon-rock.svg";
-const Container = styled.div`
-  background-color: ${(props) => props.theme.White};
-  width: 5rem;
-  height: 5rem;
+import { Link } from "react-router-dom";
+
+const Gradient = styled(Link)`
+  position: absolute;
+  background: ${(props) => props.colorM};
+  width: 11rem;
+  height: 11rem;
   border-radius: 50%;
-  padding: 3.5rem;
+  padding: 3rem;
   display: flex;
   align-items: center;
   justify-content: center;
-  border: 1.5rem solid ${(props) => props.theme.RockGradient};
+  box-shadow: 0px 5px 0px 1px ${(props) => props.color};
+  top: ${(props) => props.top};
+  left: ${(props) => props.left};
+  right: ${(props) => props.right};
+  bottom: ${(props) => props.bottom};
+
+  &:hover {
+    transform: scale(1.1);
+    transition: transform 0.2s ease-in-out;
+  }
 `;
-function Element() {
+const Container = styled.div`
+  background-color: ${(props) => props.theme.White};
+  width: 8rem;
+  height: 8rem;
+  border-radius: 50%;
+  padding: 3rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  box-shadow: 0px 5px 0px 2px #b4c1d4 inset;
+`;
+
+function Element({ img, colorBG, colorMain, top, bottom, left, right }) {
   return (
-    <Container>
-      <img src={rock} alt="" />
-    </Container>
+    <Gradient
+      to="/play"
+      color={colorBG}
+      colorM={colorMain}
+      top={top}
+      bottom={bottom}
+      left={left}
+      right={right}
+    >
+      <Container>
+        <img src={img} alt="" />
+      </Container>
+    </Gradient>
   );
 }
 
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,9 +1,17 @@
 import React from "react";
-import { Link } from "react-router-dom";
+
 import styled from "styled-components";
 import TriangleBG from "../assets/bg-triangle.svg";
 import Element from "./Element";
+import rock from "../assets/icon-rock.svg";
+import paper from "../assets/icon-paper.svg";
+import scissor from "../assets/icon-scissors.svg";
+
 const Container = styled.main`
+  width: 29rem;
+  height: 24rem;
+  position: relative;
+  margin: 10rem auto 0;
   display: grid;
   grid-template-columns: repeat(2, 1fr);
   background-image: url(${TriangleBG});
@@ -13,11 +21,28 @@ const Container = styled.main`
 function Main() {
   return (
     <Container>
-      <Link to="/play">
-        <Element />
-      </Link>
-      <Link to="/play"></Link>
-      <Link to="/play"></Link>
+      <Element
+        img={paper}
+        colorBG={"#2945c3"}
+        colorMain={() => (props) => props.theme.PaperGradient}
+        top={"0"}
+        left={"0"}
+      />
+
+      <Element
+        img={scissor}
+        colorBG={"#c76c1b"}
+        colorMain={() => (props) => props.theme.ScissorsGradient}
+        top={"0"}
+        right={"0"}
+      />
+      <Element
+        img={rock}
+        colorBG={"#991934"}
+        colorMain={() => (props) => props.theme.RockGradient}
+        left={"calc(50% - 5.5rem)"}
+        bottom={"0"}
+      />
     </Container>
   );
 }
